bffd4730e14ba4d71e1f95d6ad70f16ef0d5fcf4
Suman Adhikari <expertxkid@gmail.com>
2021-12-08 21:27:37 +0545

    Remove constructor and change curve to bounceOut

--- lib/screen/animated_box.dart
+++ lib/screen/animated_box.dart
@@ -1,7 +1,20 @@
 import 'package:flutter/material.dart';
 
-class AnimatedSquareScreen extends StatelessWidget {
+class AnimatedSquareScreen extends StatefulWidget {
   const AnimatedSquareScreen({Key? key}) : super(key: key);
+
+  @override
+  State<AnimatedSquareScreen> createState() => _AnimatedSquareScreenState();
+}
+
+class _AnimatedSquareScreenState extends State<AnimatedSquareScreen> {
+  late double squareSize;
+
+  @override
+  void initState() {
+    super.initState();
+    squareSize = 150;
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -9,13 +22,32 @@ class AnimatedSquareScreen extends StatelessWidget {
       appBar: AppBar(
         title: Text('Animated Squared Demo'),
       ),
-      body: Center(
-        child: SizedBox.square(
-          child: Container(
-            color: Colors.blue.shade400,
+      body: Column(
+        children: [
+          SizedBox(
+            height: 400,
+            child: Center(
+              child: AnimatedContainer(
+                color: Colors.blue.shade400,
+                width: squareSize,
+                height: squareSize,
+                duration: Duration(seconds: 1, milliseconds: 500),
+                curve: Curves.bounceOut,
+              ),
+            ),
           ),
-          dimension: 120,
-        ),
+          ElevatedButton(
+              onPressed: () {
+                setState(() {
+                  if (squareSize == 150) {
+                    squareSize = 300;
+                  } else {
+                    squareSize = 150;
+                  }
+                });
+              },
+              child: Text('Animate'))
+        ],
       ),
     );
   }
